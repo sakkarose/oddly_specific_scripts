11c83ab62a651e5e84eb9400e3c81bb76e5c3ab8
Dau Trong Hoang <45507664+sakkarose@users.noreply.github.com>
2024-07-11 10:28:48 +0700

    Update nessus_trial_reset.py

--- nessus_trial_reset.py
+++ nessus_trial_reset.py
@@ -68,11 +68,10 @@ def run_cmd(command):
     """Executes a command and waits for it to finish, printing output and error if any."""
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     print(f"\nCommand:\n{command}")
-    if result.returncode == 0:
-        print("Success!")
+    if result.stdout:  # Check if there's any standard output
+        return result.stdout.strip()  
     else:
-        print("Error occurred:")
-        print(result.stderr)
+        return None
 
 def is_admin():
     """Checks if the script is running with administrator privileges."""
@@ -85,33 +84,35 @@ def run_as_admin():
     """Relaunches the script with administrator privileges."""
     ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
 
-def replace_lines(file_path, replacement_lines):
-    """Replaces all lines except the first in a text file with the specified replacement lines."""
 
-    with open(file_path, 'r') as file:
-        first_line = file.readline()
-
+def write_plugin_info(version):
+    nessus_dir = r"C:\ProgramData\Tenable\Nessus"  # Define Nessus directory
+    file_path = os.path.join(nessus_dir, "nessus", "plugins", "plugin_feed_info.inc")  # Construct full path
+    
+    lines = [
+        f'PLUGIN_SET = "{version}";',
+        'PLUGIN_FEED = "ProfessionalFeed (Direct)";',
+        'PLUGIN_FEED_TRANSPORT = "Tenable Network Security Lightning";'
+    ]
+    
     with open(file_path, 'w') as file:
-        file.write(first_line)  # Write the first line back
-        for line in replacement_lines:
-            file.write(line + '\n')  # Write each replacement line with newline
+        file.write('\n'.join(lines))
     
 def main():
+    version = run_cmd("curl -s -k  https://plugins.nessus.org/v2/plugins.php")
     activation_code = generate_nessus_pro()
+
     if not is_admin():
-        print("This script requires admin privileges. Relaunching...")
         run_as_admin()
-        exit(0)  # Terminate the current non-admin process
+        return  # Exit the current process
 
     nessus_dir = r"C:\ProgramData\Tenable\Nessus"
     plugin_dir = os.path.join(nessus_dir, "nessus", "plugins")
     plugin_info_file = os.path.join(nessus_dir, "nessus", "plugin_feed_info.inc")
-    plugin_dir_info_file = os.path.join(plugin_dir, "plugin_feed_info.inc")
-    
 
     commands = [
-        'net stop "Tenable Nessus"',  
-        f'attrib -s -r -h "{plugin_dir}\\*.*"',
+        'net stop "Tenable Nessus"',
+        f'attrib -s -r -h "{plugin_dir}\\*.*"',  # Remove attributes from plugin files
         f'attrib -s -r -h "{plugin_info_file}"',
         f'nessuscli fetch --register "{activation_code}"'
     ]
@@ -119,16 +120,11 @@ def main():
     for cmd in commands:
         run_cmd(cmd)
 
-    replacement_lines = [
-        "PLUGIN_FEED = \"ProfessionalFeed (Direct)\";",
-        "PLUGIN_FEED_TRANSPORT = \"Tenable Network Security Lightning\";"
-    ]
-
-    replace_lines(plugin_info_file, replacement_lines)
+    write_plugin_info(version)
 
     commands = [
-        f'move /Y "{plugin_dir_info_file}" "{nessus_dir}\\nessus"',
-        f'attrib +s +r +h "{plugin_dir}\\*"',
+        f'move /Y "{plugin_dir}\\plugin_feed_info.inc" "{nessus_dir}\\nessus"',
+        f'attrib +s +r +h "{plugin_dir}\\*"',  # Restore attributes
         f'attrib +s +r +h "{plugin_info_file}"',
         'net start "Tenable Nessus"'
     ]
