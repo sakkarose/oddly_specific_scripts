48176251a017a9861198777d28b6da7dd77fff70
Dau Trong Hoang <ame@mizu.reisen>
2024-10-30 00:58:51 +0700

    Update subfolder_index.ps1

--- subfolder_index.ps1
+++ subfolder_index.ps1
@@ -5,4 +5,5 @@ foreach ($subFolder in $immediateSubFolders) {
     $subFolderNames += Get-ChildItem -Path $subFolder.FullName -Directory | Select-Object -ExpandProperty Name
 }
 
+$subFolderNames = $subFolderNames | Sort-Object
 $subFolderNames | Tee-Object -FilePath./index.txt
