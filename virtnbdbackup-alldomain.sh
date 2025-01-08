6596a9ac2c2778eaff2fba7354685f21f64a5968
Dau Trong Hoang <ame@mizu.reisen>
2025-01-08 17:32:37 +0700

    Create virtnbdbackup-alldomain.sh

---
+++ virtnbdbackup-alldomain.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+ROOT_DIR='/home/backup-str'
+LOG_DIR="$ROOT_DIR/log"
+UPTIME_KUMA_URL='' 
+
+get_week_number() {
+    local date="$1"
+    local week_number=$(date +%V --date="$date -$(date +%d -d "$date") days +1 day")
+    echo $(( (week_number - 1) % 4 + 1 ))
+}
+
+DATE=$(date +%Y-%m-%d)
+WEEK_NUMBER=$(get_week_number "$DATE")
+
+DOMAINS=$(virsh list --all --name)
+
+for DOMAIN in $DOMAINS; do
+    DESTINATION_DIR="$ROOT_DIR/$DOMAIN/$(date +%Y)/$(date +%m)/$WEEK_NUMBER"
+
+    # Create the destination directory if it doesn't exist
+    if [ ! -d "$DESTINATION_DIR" ]; then
+        echo "[$(date +'%Y-%m-%d %H:%M:%S')] Creating directory for domain: $DOMAIN" >> "$LOG_DIR/$DOMAIN.log" 
+        mkdir -p "$DESTINATION_DIR"
+    fi
+
+    # Dump backup information if the directory is not empty
+    if [ "$(ls -A $DESTINATION_DIR)" ]; then 
+        virtnbdrestore -i "$DESTINATION_DIR" -o dump 2>&1 | tee -a "$LOG_DIR/$DOMAIN.log"
+    else
+        echo "[$(date +'%Y-%m-%d %H:%M:%S')] Skipping 'virtnbdrestore -o dump' for $DOMAIN as the directory is empty." >> "$LOG_DIR/$DOMAIN.log"
+    fi
+
+    # Backup
+    output=$(virtnbdbackup -d "$DOMAIN" -l auto -o "$DESTINATION_DIR/" 2>&1) 
+    echo "$output" | tee -a "$LOG_DIR/$DOMAIN.log"
+
+    # Uptime Kuma backup notification
+    if echo "$output" | grep -q "Error during backup"; then
+        curl -fsS -m 10 --retry 5 -o /dev/null "$UPTIME_KUMA_URL?status=down&msg=Backup%20of%20$DOMAIN%20failed"
+    else
+        curl -fsS -m 10 --retry 5 -o /dev/null "$UPTIME_KUMA_URL?status=up&msg=Backup%20of%20$DOMAIN%20successful"
+    fi
+
+    # Verify the backup
+    verify_output=$(virtnbdrestore -i "$DESTINATION_DIR" -o verify 2>&1)
+    echo "$verify_output" | tee -a "$LOG_DIR/$DOMAIN.log"
+
+    # Failed verification = Status Down
+    if echo "$verify_output" | grep -q "Stored sums do not match"; then
+        curl -fsS -m 10 --retry 5 -o /dev/null "$UPTIME_KUMA_URL?status=down&msg=Verification%20of%20$DOMAIN%20backup%20failed" 
+    fi 
+done
